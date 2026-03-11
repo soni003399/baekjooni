801db440295af8131e455a8fe38171e01042e82f
soni003399 <hwhw003399@gmail.com>
2026-03-11 21:46:30 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+
+int h, m;
+
+scanf("%d %d", &h, &m);
+
+m= m-45;
+
+if (m<0) {m=m+60;
+h=h-1;
+
+if (h<0) {h=23;}}
+
+
+printf("%d %d\n", h, m);
+
+
+return 0;
+}
